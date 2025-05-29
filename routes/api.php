20aba77c197a76232026423dce2df496dc1f9f26
Zin Min Myat <zinminmyat@brycenmyanmar.com.mm>
2025-05-29 10:06:06 +0700

    modified

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/getUserList', [UserController::class, 'index']);
-Route::post('/changeAliceName', [UserController::class, 'changeAliceName']);
-Route::get('/getUserListApi', [UserController::class, 'index']);
-Route::post('/postUserListApi', [UserController::class, 'index']);
 
-Route::middleware([CustomAuthMiddleware::class])->group(function () {
-// Route::middleware(['auth:sanctum'])->group(function () {
-  Route::get('/myApi/extractUserListApi', [UserController::class, 'index']);  
-});
+Route::post('/user/login',[App\Http\Controllers\ApiControllers\UserRegistrationController::class,"login"]);
+Route::post('/user/register',[App\Http\Controllers\ApiControllers\LoginController::class,"register"]);
+Route::post('/place-order',[App\Http\Controllers\ApiControllers\LoginController::class,"placeOrder"]);
+Route::get('/get-orders/{id}',[App\Http\Controllers\ApiControllers\LoginController::class,"getOrdersByUserId"]);
+Route::post('/cancel-order',[App\Http\Controllers\ApiControllers\LoginController::class,"cancelOrder"]);
+Route::post('/login',[App\Http\Controllers\ApiControllers\LoginController::class,"login"]);
 
-Route::post('/login', [UserController::class, 'login']);
+// Route::middleware('auth:sanctum')->get('/get-orders/{id}', [App\Http\Controllers\ApiControllers\LoginController::class, 'getOrdersByUserId']);
+
